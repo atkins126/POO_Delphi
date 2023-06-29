ca989b399b7a2bb9db7485669bb487d5ab94a13f
Juci <jucineibelo@gmail.com>
2023-06-29 00:38:43 -0300

    aula09

---
+++ Aula 09 Polimorfismo Inherited/aula09.inherated.dpr
@@ -0,0 +1,16 @@
+program aula09.inherated;
+
+uses
+  Vcl.Forms,
+  view.principal in 'view.principal.pas' {Form1},
+  pessoa in 'pessoa.pas',
+  cliente in 'cliente.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
